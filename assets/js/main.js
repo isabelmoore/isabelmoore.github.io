08fff463ec6a63baa079465430e897d1dd860bbb
isabelmoore <isabelmoore717@tamu.edu>
2025-01-25 22:31:47 -0600

    initial blog

--- assets/js/main.js
+++ assets/js/main.js
@@ -201,3 +201,5 @@ themeButton.addEventListener("click", () => {
     localStorage.setItem("selected-theme", getCurrentTheme());
     localStorage.setItem("selected-icon", getCurrentIcon());
 });
+
+
